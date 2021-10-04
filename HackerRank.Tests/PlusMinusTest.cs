3e905d18a44278c31fbe1c91003466d8f4b693b4
Gabriel L Calsada <gabriellcalsada@gmail.com>
2021-10-04 20:20:26 -0300

    Refactor to PlusMinus resolution and test.

--- HackerRank.Tests/PlusMinusTest.cs
+++ HackerRank.Tests/PlusMinusTest.cs
@@ -8,10 +8,42 @@ namespace HackerRank.Tests
     [TestFixture]
     public class PlusMinusTest
     {
+        private List<int> arrayOfIntegers;
+
+        private decimal positivesProportion; 
+        private decimal negativesProportion;
+        private decimal zerosProportion;
+
+        [SetUp]
+        public void PlusMinusTestSetup() {
+            var randomGen = new Random();
+
+            var positiveCount = randomGen.Next(101);            
+            var negativeCount = randomGen.Next(101);
+            var zeroCount = randomGen.Next(101);
+
+            var totalCount = positiveCount + negativeCount + zeroCount;
+
+            this.positivesProportion = Convert.ToDecimal(((decimal)positiveCount/totalCount).ToString("##0.000000"));
+            this.negativesProportion = Convert.ToDecimal(((decimal)negativeCount/totalCount).ToString("##0.000000"));
+            this.zerosProportion = Convert.ToDecimal(((decimal)zeroCount/totalCount).ToString("##0.000000"));
+
+            this.arrayOfIntegers = new List<int>();
+
+            for (int i = 0; i < positiveCount; i++)
+                arrayOfIntegers.Add(randomGen.Next(1, 101));
+
+            for (int i = 0; i < negativeCount; i++)
+                arrayOfIntegers.Add(randomGen.Next(-101, -1));
+
+            for (int i = 0; i < zeroCount; i++)
+                arrayOfIntegers.Add(0);
+        }
+
+
         [Test]
         public void PlusMinus_Run_ShouldPass() {
             
-            List<int> arrayOfIntegers = new List<int>() { -1, -2, 0, 2, 1 };
             PlusMinus plusMinusChallenge = new PlusMinus();
             string result = plusMinusChallenge.Run(arrayOfIntegers);
 
@@ -21,9 +53,9 @@ namespace HackerRank.Tests
             decimal negatives = Convert.ToDecimal(results[1]);
             decimal zeros = Convert.ToDecimal(results[2]);
 
-            Assert.AreEqual(positives, 0.4);
-            Assert.AreEqual(negatives, 0.4);
-            Assert.AreEqual(zeros, 0.2);
+            Assert.AreEqual(positives, this.positivesProportion);
+            Assert.AreEqual(negatives, this.negativesProportion);
+            Assert.AreEqual(zeros, this.zerosProportion);
         }
     }
 }
--- HackerRank/PlusMinus.cs
+++ HackerRank/PlusMinus.cs
@@ -9,21 +9,37 @@ namespace HackerRank
         private decimal zeros = 0;
         private int total = 0;
 
-        public string Run(List<int> arrayOfIntegers) {
+        public string Run(List<int> arrayOfIntegers) 
+        {
             this.total = arrayOfIntegers.Count;
 
-            foreach (int number in arrayOfIntegers)
-                if(number < 0)
-                    negatives++;
-                else if(number == 0)
-                    zeros++;
-                else
-                    positives++;
+            foreach (int number in arrayOfIntegers) {
+                SumIfNegative(number);
+                SumIfPositive(number);
+                SumIfZero(number);
+            }
 
             return this.FormatOutput();
         }
 
-        public string FormatOutput() {
+        private void SumIfNegative(int number) 
+        { 
+            this.negatives += (number < 0 ? 1: 0);
+        }
+
+        private void SumIfPositive(int number) 
+        { 
+            this.positives += (number > 0 ? 1: 0);
+        }
+
+        private void SumIfZero(int number) 
+        { 
+            this.zeros += (number == 0 ? 1: 0);
+        }
+
+
+        private string FormatOutput() 
+        {
             return $"{(positives/total).ToString("##0.000000")}\n{(negatives/total).ToString("##0.000000")}\n{(zeros/total).ToString("##0.000000")}";
         }
     }
