a15f103871e0c07e2d6a7147930af9b1b39f5312
Gabriel L Calsada <gabriellcalsada@gmail.com>
2021-10-05 18:33:06 -0300

    MiniMax-Sum - Added code for failing test case (wip).

--- HackerRank/MiniMaxSum.cs
+++ HackerRank/MiniMaxSum.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,9 +8,28 @@ namespace HackerRank
     {
 
         public static string MiniMaxSumRun(List<int> arr) {
-            var fullSum = arr.Aggregate((a, b) => a + b);
-            var lowestPossibleSum = fullSum - arr.Max();
-            var biggestPossibleSum = fullSum - arr.Min();
+
+            arr = new List<int>() {256741038, 623958417, 467905213, 714532089, 938071625 };
+            long fullSum = arr.Sum<long>(arr);
+
+            // for (int i = 0; i < arr.Count; i++)
+            // {
+            //     fullSum = fullSum + arr[i];
+            // }
+            
+            long lowestPossibleSum = fullSum - arr.Max();
+            long biggestPossibleSum = fullSum - arr.Min();
+
+            return $"{lowestPossibleSum} {biggestPossibleSum}";
+        }
+
+        public static string MiniMaxSumRun_Linq(List<int> arr) {
+
+            arr = new List<int>() {256741038, 623958417, 467905213, 714532089, 938071625 };
+
+            long fullSum = arr.Aggregate((a, b) => a + b);
+            long lowestPossibleSum = fullSum - arr.Max();
+            long biggestPossibleSum = fullSum - arr.Min();
 
             return $"{lowestPossibleSum} {biggestPossibleSum}";
         }
