afcbadba308141134844bb6361cc65f9f8fbae13
g-leme-c <gabriellcalsada@gmail.com>
2021-10-06 18:20:46 -0300

    MiniMax-Sum refactor.

--- HackerRank.Tests/MiniMaxSumTest.cs
+++ HackerRank.Tests/MiniMaxSumTest.cs
@@ -5,20 +5,19 @@ using HackerRank;
 
 namespace HackerRank.Tests
 {
-
     [TestFixture]
     public class MiniMaxSumTest
     {
         [Test]
         public void MiniMaxSumTest_ShouldPass() {
-            List<int> arr = new List<int>() { 1, 3, 5, 7, 9};
+            List<int> arr = new List<int>() {256741038, 623958417, 467905213, 714532089, 938071625};
 
             var result = MiniMaxSum.MiniMaxSumRun(arr);
 
             var resultArray = result.Split(" ");
 
-            Assert.AreEqual(resultArray[0], "16");
-            Assert.AreEqual(resultArray[1], "24");
+            Assert.AreEqual(resultArray[0], "2063136757");
+            Assert.AreEqual(resultArray[1], "2744467344");
         }
     }
 }
--- HackerRank/MiniMaxSum.cs
+++ HackerRank/MiniMaxSum.cs
@@ -6,34 +6,18 @@ namespace HackerRank
 {
     public class MiniMaxSum
     {
-
         public static string MiniMaxSumRun(List<int> arr) {
-
-            arr = new List<int>() {256741038, 623958417, 467905213, 714532089, 938071625 };
-            long fullSum = arr.Sum<long>(arr);
-
-            // for (int i = 0; i < arr.Count; i++)
-            // {
-            //     fullSum = fullSum + arr[i];
-            // }
+            long fullSum = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                fullSum = fullSum + arr[i];
+            }
             
             long lowestPossibleSum = fullSum - arr.Max();
             long biggestPossibleSum = fullSum - arr.Min();
 
             return $"{lowestPossibleSum} {biggestPossibleSum}";
-        }
-
-        public static string MiniMaxSumRun_Linq(List<int> arr) {
-
-            arr = new List<int>() {256741038, 623958417, 467905213, 714532089, 938071625 };
-
-            long fullSum = arr.Aggregate((a, b) => a + b);
-            long lowestPossibleSum = fullSum - arr.Max();
-            long biggestPossibleSum = fullSum - arr.Min();
-
-            return $"{lowestPossibleSum} {biggestPossibleSum}";
-        }
-        
+        }        
     }
     
 }
